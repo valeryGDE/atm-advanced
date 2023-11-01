2f9f50f7ec8a3d06e97d66c4dec2e4fd9742210e
“Valeryia <valeryia_kazlova@epam.com>
2023-11-01 20:33:59 +0100

    update

--- src/main/java/service/LoginService.java
+++ src/main/java/service/LoginService.java
@@ -5,6 +5,9 @@ import main.java.core.logger.Log;
 import main.java.core.properties.EnvProperty;
 import main.java.core.properties.PropertyReader;
 import main.java.pagefactory.page.LoginPage;
+import org.openqa.selenium.support.ui.Sleeper;
+
+import java.time.Duration;
 
 public class LoginService {
 
@@ -15,10 +18,11 @@ public class LoginService {
         loginPage.clickLoginEpamButton();
     }
 
-    public void logIn(Account account) {
+    public void logIn(Account account) throws InterruptedException {
         Log.info(String.format("Log in using account: '%s'", account.getEmail()));
         LoginPage loginPage = new LoginPage();
         loginPage.openPage(PropertyReader.getProperty(EnvProperty.LOCAL_BASE_URL.getKey()));
+        Sleeper.SYSTEM_SLEEPER.sleep(Duration.ofSeconds(10));
         loginPage.fillEmailInput(account.getEmail());
         loginPage.fillPasswordInput(account.getPassword());
         loginPage.clickLoginButton();
--- src/test/java/test/LoginTest.java
+++ src/test/java/test/LoginTest.java
@@ -1,23 +1,17 @@
 package test;
 
 import main.java.businesslogic.manager.AccountManager;
-import main.java.pagefactory.page.LoginPage;
 import main.java.service.LoginService;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.Sleeper;
 import org.testng.annotations.Test;
-
-import java.time.Duration;
 
 public class LoginTest extends BaseTest {
 
     @Test
-    public void loginToRemoteReportPortal() throws InterruptedException {
+    public void loginToLocalReportPortal() throws InterruptedException {
         LoginService loginService = new LoginService();
-        Sleeper.SYSTEM_SLEEPER.sleep(Duration.ofSeconds(5));
         loginService.logIn(AccountManager.defaultAccount());
-        Sleeper.SYSTEM_SLEEPER.sleep(Duration.ofSeconds(5));
     }
 
     @Test
